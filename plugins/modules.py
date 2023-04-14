b96813a79ea2b07da2e4481d3f7d4e6747a7b68b
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-14 14:18:45 +0530

    All bugs fixed😌

--- plugins/modules.py
+++ plugins/modules.py
@@ -1,6 +1,6 @@
 import time
 import random
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 from Script import script
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
 
