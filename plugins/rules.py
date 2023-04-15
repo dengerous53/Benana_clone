57cf992219251a3345d1bbf4966040e7754f7214
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-15 18:52:05 +0530

    Update rules.py

--- plugins/rules.py
+++ plugins/rules.py
@@ -5,3 +5,20 @@ from Script import script
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
 
 CMD = ["/", "."]
+
+
+@Client.on_message(filters.command(["rules"]))
+async def help(client, message):
+        buttons = [[
+                    InlineKeyboardButton('𝚁𝚄𝙻𝙴𝚂', callback_data="rule"),
+                  ]]
+        
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await client.send_photo(
+           photo=random.choice(ADD_ME),
+           caption=script.RULES_TXT,
+           chat_id=message.chat.id,
+           reply_markup=reply_markup,
+           parse_mode=enums.ParseMode.HTML,
+           reply_to_message_id=message.id
+       )
