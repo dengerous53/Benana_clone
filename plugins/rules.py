0378cbcb1ba9d52954acd8a1d57d1832947dce98
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-15 18:51:11 +0530

    Create rules.py

---
+++ plugins/rules.py
@@ -0,0 +1,7 @@
+import time
+import random
+from pyrogram import Client, filters, enums
+from Script import script
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
+
+CMD = ["/", "."]
