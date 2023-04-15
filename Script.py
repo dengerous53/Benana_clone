782b119db3b6cec24d68e0f80b1c449e9db752d1
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-15 17:56:35 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -273,13 +273,9 @@ Nᴀᴍᴇ - {}"""
 <b>📂 Fɪʟᴇ ɴᴀᴍᴇ : </b> <code>{file_name}</code>
 
 <b>
-╭─────── • ◆ • ───────╮
-🔅 Dᴀʀᴋ Mᴏᴅᴇ :  <a href="https://t.me/addtheme/DQ_The_File_Donor_Theme">Tᴏᴜᴄʜ</a>
-╰─────── • ◆ • ───────╯
-
 =========== • ✠ • ===========
-▫️ ᴄʜᴀɴɴᴇʟ : @DQ_The_File_Donor_Updates
-▫️ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ : @DQ_The_File_Donor_Support
+▫️ ᴄʜᴀɴɴᴇʟ : @LSBOTZ_UPDATE
+▫️ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ : @EDIT_REPO
 =========== • ✠ • ===========</b>"""
 
     IMDB_TEMPLATE_TXT = """
