926443df5d0cb5c451393180622a4f9ccbf3f26a
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-15 19:26:50 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -164,4 +164,11 @@ class Database:
     async def get_db_size(self):
         return (await self.db.command("dbstats"))['dataSize']
 
+    async def set_caption(self, id, caption):
+        await self.col.update_one({'id': int(id)}, {'$set': {'caption': caption}})
+
+    async def get_caption(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('caption', None)
+
 db = Database(DATABASE_URI, DATABASE_NAME)
