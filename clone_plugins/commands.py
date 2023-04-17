38c3944a159f4bcf3af59916931ccb0a22d69b72
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-17 19:05:16 +0530

    Update commands.py

--- clone_plugins/commands.py
+++ clone_plugins/commands.py
@@ -61,6 +61,7 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML
         )
         return
+        bot = await get_bot() 
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
