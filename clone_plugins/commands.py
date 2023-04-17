a6feaffb827b9e76ebeccf5281df158d39a9e9a6
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-17 10:23:30 +0530

    Update commands.py

--- clone_plugins/commands.py
+++ clone_plugins/commands.py
@@ -18,8 +18,6 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
-bot = await ai.get_me()
-
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
