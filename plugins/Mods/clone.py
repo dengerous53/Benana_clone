5f3633ec72160d8b6c5920d65510fee35e70bdf6
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2023-04-18 07:52:37 +0530

    Update clone.py

--- plugins/Mods/clone.py
+++ plugins/Mods/clone.py
@@ -14,7 +14,7 @@ from info import DATABASE_URI as MONGO_URL
 mongo_client = MongoClient(MONGO_URL)
 mongo_db = mongo_client["cloned_bots"]
 
-class clone(object):
+class clonedme(object):
     ME = None
     U_NAME = None
     B_NAME = None
@@ -47,9 +47,9 @@ async def on_clone(self, message):
                     'username': bot.username
                 }
                 mongo_db.bots.insert_one(details)
-                clone.ME = bot.id
-                clone.U_NAME = bot.username
-                clone.B_NAME = bot.first_name
+                clonedme.ME = bot.id
+                clonedme.U_NAME = bot.username
+                clonedme.B_NAME = bot.first_name
                 await msg.edit_text(f"𝚂𝚞𝚌𝚌𝚎𝚜𝚏𝚞𝚕𝚕𝚢 𝙲𝚕𝚘𝚗𝚎𝚍 𝚢𝚘𝚞𝚛 @{bot.username} .\n\n⚠️ <u>𝙳𝚘 𝙽𝚘𝚝 𝚂𝚎𝚗𝚍 𝚃𝚘 𝙰𝚗𝚢 𝙾𝚗𝚎</u> 𝚃𝚑𝚎 𝙼𝚎𝚜𝚜𝚊𝚐𝚎 𝚆𝚒𝚝𝚑 <u>𝚃𝚑𝚎 𝚃𝚘𝚔𝚎𝚗</u> 𝙾𝚏 𝚃𝚑𝚎 𝙱𝚘𝚝, 𝚆𝚑𝚘 𝙷𝚊𝚜 𝙸𝚝 𝙲𝚊𝚗 𝙲𝚘𝚗𝚝𝚛𝚘𝚕 𝚈𝚘𝚞𝚛 𝙱𝚘𝚝!\n<i>𝙸𝚏 𝚈𝚘𝚞 𝚃𝚑𝚒𝚗𝚔 𝚂𝚘𝚖𝚎𝚘𝚗𝚎 𝙵𝚘𝚞𝚗𝚍 𝙾𝚞𝚝 𝙰𝚋𝚘𝚞𝚝 𝚈𝚘𝚞𝚛 𝙱𝚘𝚝 𝚃𝚘𝚔𝚎𝚗, 𝙶𝚘 𝚃𝚘 @Botfather, 𝚄𝚜𝚎 /revoke 𝙰𝚗𝚍 𝚃𝚑𝚎𝚗 𝚂𝚎𝚕𝚎𝚌𝚝 @{bot.username}</i>")
             except BaseException as e:
                 logging.exception("Error while cloning bot.")
